5a102425151183dd7842089d3a3d3239dde49a9a
fredrickroyal <kfredrick35@gmail>
2022-12-17 14:35:33 +0300

    initial code

---
+++ src/main/java/com/swizima/reportengine/ReportEngineApplication.java
@@ -0,0 +1,13 @@
+package com.swizima.reportengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReportEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swizima/reportengine/ReportEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swizima.reportengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
